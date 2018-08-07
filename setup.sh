cf696ae9755fc582a658ae5d6c59849c22c8c3b6
Sudeep Gupta <sudeepgupta90@gmail.com>
2018-08-07 19:41:50 +0530

    cleaned up the code

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ echo "Setup Ansible Dev Environment!"
 
 if [ $# -eq 0 ]
 then
-	echo "specify git repo to clone, and setup ansible dev environment!"
-	exit 1
+    echo "specify git repo to clone, and setup ansible dev environment!"
+    exit 1
 fi
 
 ANSIBLE_GIT=$1
@@ -15,15 +15,14 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 if [ ! -d "$PYTHON_VENV" ]
 then
     python3 -m venv $PYTHON_VENV
-    # echo $project_folder + "/"+ $python_venv
 fi
 
 rm -rf ./DockerAnsibleContainerAutomation
 
 if [ ! -d "DockerAnsibleContainerAutomation" ]
 then
-	GIT_REPO="git@github.com:sudeepgupta90/DockerAnsibleContainerAutomation.git"
-	git clone $GIT_REPO
+    GIT_REPO="git@github.com:sudeepgupta90/DockerAnsibleContainerAutomation.git"
+    git clone $GIT_REPO
 fi
 
 mv ./DockerAnsibleContainerAutomation/launchContainers.py ./
@@ -41,9 +40,9 @@ pip install -r requirements.txt
 echo "setting up ansible directory"
 if [ ! -d "ansible" ]
 then
-	mkdir ./ansible
+    mkdir ./ansible
 fi
 
+echo "git clone ansible repo ..."
 cd ansible
-
-git clone $1
+git clone $ANSIBLE_GIT
